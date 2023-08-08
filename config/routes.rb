2a23b5c843f889e620a7ae11d9d494d51861f656
LeanTrey <treymclean0003@gmail.com>
2023-08-08 12:20:27 -0400

    Initial commit
    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_for :models
+  root "articles#index"
+  
+  resources :articles do
+    resources :comments
+  end
+  resources :comments
+end
